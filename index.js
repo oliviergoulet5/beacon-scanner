585f6e5fe1a50bfdb08ae0c069d94aba6b43038e
Olivier Goulet <oliviergoulet5@gmail.com>
2020-02-15 07:58:44 -0500

    calculateDistance added
    Does not have the proper arguments

--- index.js
+++ index.js
@@ -3,9 +3,21 @@ const scanner = new BeaconScanner();
 
 console.log(scanner);
 
+function calculateDistance(rssi, txPowerNeg) {
+    const N = 3; // ((txPower – rssi) / (10 * N))
+    let txPower = txPowerNeg * -1;
+    return Math.pow(10, (txPower - rssi) / (10 * N));
+}
+
 scanner.onadvertisement = (ad) => {
-    if (ad.beaconType != 'iBeacon')
+    if (ad.beaconType != 'iBeacon') {
         console.log(JSON.stringify(ad, null, '  '));
+        console.log(ad.id);
+    }
+    
+    console.log("Distance: ");
+    if (ad.rssi !== undefined && ad.eddystoneUrl.txPower !== undefined)
+        console.log(calculateDistance(ad.rssi, -21));
 };
 
 scanner.startScan().then(() => {
@@ -13,3 +25,18 @@ scanner.startScan().then(() => {
 }).catch((error) => {
     console.error(error);
 });
+
+/*
+{
+  "id": "c6debed8f549",
+  "address": "c6:de:be:d8:f5:49",
+  "localName": null,
+  "txPowerLevel": null,
+  "rssi": -59, // signal strength
+  "beaconType": "eddystoneUrl",
+  "eddystoneUrl": {
+    "txPower": -35,
+    "url": "http://go.esti.be/"
+  }
+}
+*/
